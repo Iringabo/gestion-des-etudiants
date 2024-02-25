f5673cf9a5df49df4f1f8d0ce0f8f8948ec0246f
Iringabo <iringalinne@gmail.com>
2024-02-25 21:59:17 +0200

    version 3

--- app/forms.py
+++ app/forms.py
@@ -17,10 +17,10 @@ class SignupForm(UserCreationForm):
             ('username', 'Nom', 'Prenom', 'email', 'is_professeur')
 
     def try_save(self, request):
-        user = super(SignupForm, self).save(commit=False)
+        user = super.save(commit=False)
         user.set_password(self.cleaned_data["password1"])
         user.save()
-        return user
+        return user,None
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
